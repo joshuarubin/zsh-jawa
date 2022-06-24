f35e96705411959eec351f225f8e7a5b3a08f616
Joshua Rubin <me@jawa.dev>
2022-06-23 21:41:33 -0600

    add support for neovim/wezterm interaction
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- startup/02_editors.zsh
+++ startup/02_editors.zsh
@@ -6,5 +6,11 @@ fi
 if (( $#commands[nvim] )); then
   export EDITOR="nvim"
   export VISUAL="nvim"
+
+  local args="$(nvim_args)"
+
+  if [ -n "${args}" ]; then
+    alias nvim="nvim ${args}"
+  fi
 fi
 
